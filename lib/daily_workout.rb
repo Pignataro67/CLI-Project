9174e2a55bfe064d9dbe347cfd0cc58c3b816256
Pat  Pignataro <pat6284@msn.com>
2017-09-06 16:37:57 +0000

    adding scraping requirements and pry

--- lib/daily_workout.rb
+++ lib/daily_workout.rb
@@ -1,2 +1,7 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
 require_relative "./daily_workout/version"
+require_relative './daily_workout/exercise_workout'
 require_relative './daily_workout/cli'
