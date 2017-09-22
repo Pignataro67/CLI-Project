84007aa226b96d40dd4a3fa926bb0ec203a0844b
Pat  Pignataro <pat6284@msn.com>
2017-09-22 21:42:56 +0000

    Adding a fourth exercise

--- lib/daily_workout/exercise_workout.rb
+++ lib/daily_workout/exercise_workout.rb
@@ -11,6 +11,7 @@ class DailyWorkout::Exercise_workout
     workouts << self.scrape_body_building_1
     workouts << self.scrape_body_building_2
     workouts << self.scrape_body_building_3
+    workouts << self.scrape_body_building_4
 
     workouts
   end
@@ -21,11 +22,10 @@ class DailyWorkout::Exercise_workout
     workout = self.new
     workout.name = doc.search("h1.main-header").text.strip
     workout.exercise = doc.search("h4.dpg-h4").first.text.strip
-    workout.sets_and_reps = "2 warm-up sets of 8-10 reps followed by 5 working sets of 8-10 reps"
+    workout.sets_and_reps = "2 warm-up sets of 5-8 reps followed by 5 working sets of 3-5 reps (Increase the weight each set)"
     workout.url = "https://bodybuilding.com/fun/the-back-day-motivation-you-didnt-know-you-needed.html"
 
     workout
-
   end
 
   def self.scrape_body_building_2
@@ -34,7 +34,7 @@ class DailyWorkout::Exercise_workout
     workout = self.new
     workout.name = doc.search("h1.main-header").text.strip
     workout.exercise = doc.search("h3.dpg-h3")[1].text.strip
-    workout.sets_and_reps = "5 sets of 12, 10, 8, 6, 4 reps"
+    workout.sets_and_reps = "5 sets of 12, 10, 8, 6, 4 reps (Increase the weight each set)"
     workout.url = "https://bodybuilding.com/fun/build-a-chest-that-stands-out-from-every-angle.html"
 
     workout
@@ -46,8 +46,20 @@ class DailyWorkout::Exercise_workout
     workout = self.new
     workout.name = doc.search("h1.main-header").text.strip
     workout.exercise = doc.search("h4.dpg-h4 a")[1].text.strip
-    workout.sets_and_reps = "4 sets, 6-8, 6-8, 8-10, 8-10 reps (Lighten the weight after your first 2 sets)"
+    workout.sets_and_reps = "5 sets, 10 reps"
     workout.url = "https://www.bodybuilding.com/fun/sculpt-your-lower-body-with-erin-stern.html"
+
+    workout
+  end
+
+  def self.scrape_body_building_4
+    doc = Nokogiri::HTML(open("https://www.bodybuilding.com/fun/supercharge-your-shoulders-with-this-weird-exercise.html"))
+
+    workout = self.new
+    workout.name = doc.search("h1.main-header").text.strip
+    workout.exercise = doc.search("h4.dpg-h4 a")[0].text.strip
+    workout.sets_and_reps = "4 sets, 10, 10, 8, 8 reps (Increase the weight after your first 2 sets)"
+    workout.url = "https://www.bodybuilding.com/fun/supercharge-your-shoulders-with-this-weird-exercise.html"
 
     workout
   end
