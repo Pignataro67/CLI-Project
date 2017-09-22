6c37612c3fc93f783bd2cfedf6aa80d70c85d018
Pat  Pignataro <pat6284@msn.com>
2017-09-22 19:24:02 +0000

    adding more functionality to the 3rd exercise

--- lib/daily_workout/exercise_workout.rb
+++ lib/daily_workout/exercise_workout.rb
@@ -10,6 +10,7 @@ class DailyWorkout::Exercise_workout
 
     workouts << self.scrape_body_building_1
     workouts << self.scrape_body_building_2
+    workouts << self.scrape_body_building_3
 
     workouts
   end
@@ -18,24 +19,37 @@ class DailyWorkout::Exercise_workout
     doc = Nokogiri::HTML(open("https://bodybuilding.com/fun/the-back-day-motivation-you-didnt-know-you-needed.html"))
 
     workout = self.new
-    workout.name = doc.search("h1.main-header").text
-    workout.exercise = doc.search("div.dpgpt-content a").text
+    workout.name = doc.search("h1.main-header").text.strip
+    workout.exercise = doc.search("h4.dpg-h4").first.text.strip
     workout.sets_and_reps = "2 warm-up sets of 8-10 reps followed by 5 working sets of 8-10 reps"
     workout.url = "https://bodybuilding.com/fun/the-back-day-motivation-you-didnt-know-you-needed.html"
 
     workout
-    #binding.pry
+
   end
 
   def self.scrape_body_building_2
     doc = Nokogiri::HTML(open("https://bodybuilding.com/fun/build-a-chest-that-stands-out-from-every-angle.html"))
 
     workout = self.new
-    workout.name = doc.search("h1.main-header").text
-    workout.exercise = doc.search("h3.dpg-h3").text
+    workout.name = doc.search("h1.main-header").text.strip
+    workout.exercise = doc.search("h3.dpg-h3")[1].text.strip
     workout.sets_and_reps = "5 sets of 12, 10, 8, 6, 4 reps"
     workout.url = "https://bodybuilding.com/fun/build-a-chest-that-stands-out-from-every-angle.html"
 
     workout
   end
+
+  def self.scrape_body_building_3
+    doc = Nokogiri::HTML(open("https://www.bodybuilding.com/fun/sculpt-your-lower-body-with-erin-stern.html"))
+
+    workout = self.new
+    workout.name = doc.search("h1.main-header").text.strip
+    workout.exercise = doc.search("h4.dpg-h4 a")[1].text.strip
+    workout.sets_and_reps = "4 sets, 6-8, 6-8, 8-10, 8-10 reps (Lighten the weight after your first 2 sets)"
+    workout.url = "https://www.bodybuilding.com/fun/sculpt-your-lower-body-with-erin-stern.html"
+
+    workout
+  end
+
 end
