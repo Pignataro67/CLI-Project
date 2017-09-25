623945f8c5b15bcefabce99029b815463878ea59
Pat  Pignataro <pat6284@msn.com>
2017-09-25 17:56:32 +0000

    improving the functionality of the CLI in list form and detail form

--- lib/daily_workout/cli.rb
+++ lib/daily_workout/cli.rb
@@ -10,7 +10,7 @@ class DailyWorkout::CLI
     puts "Today's Workouts:"
     @workouts = DailyWorkout::Exercise_workout.today
     @workouts.each.with_index(1) do |workout, x|
-      puts "#{x}. #{workout.name} - #{workout.exercise} - #{workout.sets_and_reps}"
+      puts "#{x}. #{workout.name}"
     end
   end
 
@@ -20,18 +20,21 @@ class DailyWorkout::CLI
         puts "Please select the desired workout by number or type list to see the workouts again or type exit to exit:"
       input = gets.strip.downcase
 
-      if input.to_i.between?(1, 4)
-      the_workout = @workouts[input.to_i-1]
+      if input.to_i.between?(1, 5)
+        the_workout = @workouts[input.to_i-1]
         puts  "#{the_workout.name} - #{the_workout.exercise} - #{the_workout.sets_and_reps}"
       elsif input == "list"
         list_workouts
+      elsif input == "exit"
       else
-        puts "Pick a workout or I will crush you, like a little walnut!!!"
+        puts "Don't you want to workout?!?"
+        sleep(2)
+        puts "IT DOESN'T MATTER IF YOU WANT TO WORKOUT, you're going to workout, IF YOU SMELLLL, WHAT P, A, T, IS COOKIN'!!!"
       end
     end
   end
 
   def goodbye
-    puts "See you tomorrow for more heart pounding, back breaking workouts!"
+      puts "See you tomorrow for more heart pounding, back breaking workouts!"
   end
 end
