544834ea229cf8ecb275381814fa8ebb29d7e568
Pat  Pignataro <pat6284@msn.com>
2017-10-10 18:51:48 +0000

    adding the requiring of './daily_workout/scraper' to this file.

--- lib/daily_workout.rb
+++ lib/daily_workout.rb
@@ -4,3 +4,4 @@ require 'nokogiri'
 require_relative "./daily_workout/version"
 require_relative './daily_workout/exercise_workout'
 require_relative './daily_workout/cli'
+require_relative './daily_workout/scraper'
